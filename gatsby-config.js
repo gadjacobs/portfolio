479ed1e8115d9e1f398e65362f0fead6361e3e78
gadjacobs <gadexofficial@gmail.com>
2018-12-08 14:27:21 +0100

    Updated gatsby config

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gad Jacobs`,
+    siteUrl: `https://www.gadjacobs.me`,
+    description: `My Personal Portfolio`
+  },
+  plugins: [`gatsby-plugin-netlify-cms`],
+};
