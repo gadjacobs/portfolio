13765432da7b1e5e8621b2296c3d543046cb987f
gadjacobs <gadexofficial@gmail.com>
2018-12-09 05:47:00 +0100

    Removed bloatware

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,19 @@
-import React from "react"
+import React from 'react';
+import Helmet from 'react-helmet';
 
 export default ({ children }) => (
-  <div style={{ margin: `0 auto` }}>
+  <div>
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700"
+        rel="stylesheet"
+        type="text/css"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700"
+        rel="stylesheet"
+      />
+    </Helmet>
     {children}
   </div>
-)
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Header from '../components/header';
 
 export default () => (
   <Layout>
-    <div>
-      My Portfolio
+    <div id="page">
+      <Header></Header>
     </div>
   </Layout>
 );
