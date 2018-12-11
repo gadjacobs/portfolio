e8a3c71e200a9e2ee98803a4e3f3a9ef930b3f28
gadjacobs <gadexofficial@gmail.com>
2018-12-11 02:50:02 +0100

    Modified header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import 'tachyons';
 import './header.css';
+import SocialLinks from './social-links';
+import Logo from './logo'
 
 const Header = () => {
   return (
@@ -17,17 +19,9 @@ const Header = () => {
               <div className="dtc w2 v-mid pa3">
                 <a
                   href="/"
-                  className="dib w2 h2 pa1 ba b--white-90 grow-large border-box"
+                  className="dib w3 h3 b--white-90 grow-large"
                 >
-                  <svg
-                    className="link white-90 hover-white"
-                    data-icon="skull"
-                    viewBox="0 0 32 32"
-                    style={{ fill: `currentcolor` }}
-                  >
-                    <title>Gad Jacobs</title>
-                    <path d="M16 0 C6 0 2 4 2 14 L2 22 L6 24 L6 30 L26 30 L26 24 L30 22 L30 14 C30 4 26 0 16 0 M9 12 A4.5 4.5 0 0 1 9 21 A4.5 4.5 0 0 1 9 12 M23 12 A4.5 4.5 0 0 1 23 21 A4.5 4.5 0 0 1 23 12" />
-                  </svg>
+                  <Logo />
                 </a>
               </div>
               <div className="dtc v-mid tr pa3">
@@ -62,7 +56,7 @@ const Header = () => {
               <h3 className="fw1 f3 white-80 mt3 mb4">
                 fullstack web developer | JavaScript x DotNet
               </h3>
-
+<SocialLinks/>
               {/* <div class="pa4-l">
   <form class="bg-light-red mw7 center pa4 br2-ns ba b--black-10">
     <fieldset class="cf bn ma0 pa0">
