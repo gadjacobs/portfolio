60a1b7bd095d3b00279dcf770aa652b10b118f57
gadjacobs <gadexofficial@gmail.com>
2018-12-11 02:49:05 +0100

    Udated head links and meta's

--- src/html.js
+++ src/html.js
@@ -13,7 +13,7 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           {this.props.headComponents}
-          
+
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
